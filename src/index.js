7ba3af5bfd3753cbabf537879bd3aca635aac822
ArnabhS <sinhaarnabh888@gmail.com>
2024-10-15 22:55:17 +0530

    analysing disorder functionality implemented with cohere AI

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const connectDB = require('./config/db.js');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/authRoutes.js')
+const testRoutes = require('./routes/testRoutes.js')
 const app = express();
 
 dotenv.config();
@@ -20,6 +21,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/v1/auth',authRoutes)
+app.use('/api/v1/test',testRoutes)
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
