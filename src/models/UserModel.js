db35e3baa47ee6fd6392fbb27e355a6ad2dd25a5
ArnabhS <sinhaarnabh888@gmail.com>
2024-10-15 12:28:39 +0530

    Initial setup and Db connected

---
+++ src/models/UserModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required: true,
+        unique: true,
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+    loginCount: { 
+        type: Number, 
+        default: 0 
+    },
+    promptsUsed: { 
+        type: Number, 
+        default: 0 
+    },  
+    questionsAnswered: { 
+        type: Number, 
+        default: 0 
+    },  
+    testResults: {
+      adhdScore: { 
+        type: Number, 
+        default: 0 
+    },
+      autismScore: { 
+        type: Number, 
+        default: 0 
+    },
+      dyslexiaScore: {
+        type: Number, 
+        default: 0 
+    },
+    },
+    detectedDisorders: [{ type: String }],  
+    answers: [{ 
+        questionId: mongoose.Schema.Types.ObjectId, 
+        answer: String 
+    }],  
+    dateTaken: { type: Date, default: Date.now }
+})
